42fe2e4208c6cbf126f15b1412a35188bac881b7
DrunkScienceMan <131479467+DrunkScienceMan@users.noreply.github.com>
2025-10-02 22:07:05 +0200

    Update helper.php

--- perlite/helper.php
+++ perlite/helper.php
@@ -381,7 +381,7 @@ function getFileInfos($file)
 	// Remove extension for both .md and .pdf files
 	if (strcmp(substr($mdFile, -3), ".md") === 0) {
 		$mdFile = substr($mdFile, 0, -3);
-	
+	}
 
 	$folderClean = str_replace('$' . $rootDir, '', '$' . pathinfo($file)["dirname"]);
 
